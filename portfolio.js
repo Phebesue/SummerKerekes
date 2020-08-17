e6f22abbbccd38232cfb5f263fe254eb1ded5cf5
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-08-16 21:16:47 -0400

    Add modals to project section

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+
+// https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
